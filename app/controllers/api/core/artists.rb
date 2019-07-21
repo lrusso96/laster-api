e3ecc3da80114de19966947704abc7b2ee1a589f
luigi <russo.1699981@studenti.uniroma1.it>
2019-07-21 18:16:02 +0200

    get similar artists

--- app/controllers/api/core/artists.rb
+++ app/controllers/api/core/artists.rb
@@ -26,13 +26,15 @@ module Laster
       tracks
     end
 
-    def self.similar(artist)
-      # TODO: implement method
-      artists = []
-      1.upto 10 do |value|
-        artists << Artist.new(name: "recent artist - #{value}")
-      end
-      artists
+    def self.similar(artist, limit: nil)
+      params = { method: 'artist.getsimilar', api_key: ENV['LASTFM_API_KEY'],
+                 format: 'json' }
+      # I don't trust the caller
+      args = { artist: artist, limit: limit }.compact
+      params.reverse_merge!(args)
+
+      opt = { query: params, headers: { 'User-Agent' => ENV['USER_AGENT'] } }
+      parse_similar JSON.parse HTTParty.get(API_ENDPOINT, opt).body
     end
 
     private_class_method def self.parse_search(res)
@@ -52,5 +54,15 @@ module Laster
       code = res['error']
       raise Laster::Errors::Simple.new(msg: res['message'], code: code) if code
     end
+
+    private_class_method def self.parse_similar(res)
+     catch_error res
+     ret = []
+     artists = res['similarartists']['artist']
+     artists.each do |a|
+       ret << Artist.new(name: a['name'])
+     end
+     ret
+   end
   end
 end
