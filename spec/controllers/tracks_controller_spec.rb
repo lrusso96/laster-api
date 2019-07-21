a8dcd03ff6951bf9360d7ee67d5f8f4ac982291d
luigi <russo.1699981@studenti.uniroma1.it>
2019-07-21 11:05:52 +0200

    fix many tests

--- spec/controllers/tracks_controller_spec.rb
+++ spec/controllers/tracks_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe TracksController, type: :controller do
-
 end
--- spec/requests/tracks_spec.rb
+++ spec/requests/tracks_spec.rb
@@ -2,111 +2,153 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Laster::Tracks API', type: :request do
-  # initialize test data
-  let!(:tracks) { create_list(:track, 10) }
-  let(:valid_attributes) { { track: 'Wonderwall' } }
-  let(:invalid_attributes) { { wrong_par: 'Searched Song' } }
-  let(:info_attributes) { { track: 'The wall', artist: 'Pink Floyd' } }
-  let(:invald_info_attributes) { { track: 'The wall' } }
+# Test suite for GET /tracks?track=song&{artist=a}
+RSpec.describe 'Laster::Tracks.search', type: :request do
+  let(:valid_attr) { { track: 'Wonderwall' } }
+  let(:limit) { 5 }
+  let(:full_attr) { { track: 'Smells like teen spirit', artist: 'Nirvana', limit: limit } }
+  let(:invalid_attr) { { wrong_par: 'Searched Song' } }
 
-  # Test suite for GET /tracks/top
-  describe 'GET top songs' do
-    # make HTTP get request before each example
+  context 'when at least TRACK parameter is present' do
+    before { get '/tracks', params: valid_attr }
+
+    it 'returns the result of the search' do
+      expect(json).not_to be_empty
+      # assume 5 tracks at least
+      expect(json.size).to be > 5
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  context 'when TRACK and optional parameters are present' do
+    before { get '/tracks', params: full_attr }
+
+    it 'returns the result of the search' do
+      expect(json).not_to be_empty
+      # assume 5 tracks at least
+      expect(json.size).to eq(limit)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  context 'when track parameter is NOT present' do
+    before { get '/tracks', params: invalid_attr }
+
+    it 'returns status code 400' do
+      expect(response).to have_http_status(400)
+    end
+
+    it 'returns a validation failure message' do
+      expect(response.body)
+        .to match(/param is missing or the value is empty: track/)
+    end
+  end
+end
+
+RSpec.describe 'Laster::Tracks.info', type: :request do
+  let(:valid_attr) { { track: 'The wall', artist: 'Pink Floyd' } }
+  let(:invalid_attr) { { track: 'The wall' } }
+
+  context 'when both TRACK and ARTIST parameters are present' do
+    before { get '/tracks/info', params: valid_attr }
+
+    it 'returns the result of the search' do
+      expect(json).not_to be_empty
+      # FIXME: parse result to see details!
+      expect(json.size).to eq(1)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  context 'when a required parameter is NOT present' do
+    before { get '/tracks/info', params: invalid_attr }
+
+    it 'returns status code 400' do
+      expect(response).to have_http_status(400)
+    end
+
+    it 'returns a validation failure message' do
+      expect(response.body)
+        .to match(/param is missing or the value is empty: artist/)
+    end
+  end
+end
+
+RSpec.describe 'Laster::Tracks.similar', type: :request do
+  let(:valid_attr) { { track: 'The wall', artist: 'Pink Floyd' } }
+  let(:invalid_attr) { { track: 'The wall' } }
+
+  context 'when both TRACK and ARTIST parameters are present' do
+    before { get '/tracks/info', params: valid_attr }
+
+    it 'returns the result of the search' do
+      expect(json).not_to be_empty
+      # FIXME: parse result to see details!
+      expect(json.size).to be > 0
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  context 'when a required parameter is NOT present' do
+    before { get '/tracks/info', params: invalid_attr }
+
+    it 'returns status code 400' do
+      expect(response).to have_http_status(400)
+    end
+
+    it 'returns a validation failure message' do
+      expect(response.body)
+        .to match(/param is missing or the value is empty: artist/)
+    end
+  end
+end
+
+# Test suite for GET /tracks/top
+RSpec.describe 'Laster::Tracks.top', type: :request do
+  let!(:tracks) { create_list(:track, 10) }
+
+  context 'default case' do
     before { get '/tracks/top' }
 
-    context 'default case' do
-      it 'returns the top charts (10 songs now!)' do
-        expect(json).not_to be_empty
-        # remove this line after the implementation of search function!
-        expect(json.size).to eq(10)
-      end
+    it 'returns the top charts (10 songs now!)' do
+      expect(json).not_to be_empty
+      # remove this line after the implementation of search function!
+      expect(json.size).to eq(10)
+    end
 
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
-      end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
     end
   end
+end
 
-  # Test suite for GET /tracks/latest
-  describe 'GET latest songs' do
-    # make HTTP get request before each example
+# Test suite for GET /tracks/latest
+RSpec.describe 'Laster::Tracks.latest', type: :request do
+  let!(:tracks) { create_list(:track, 10) }
+
+  context 'default case' do
     before { get '/tracks/latest' }
 
-    context 'default case' do
-      it 'returns the latest charts (100 songs now!)' do
-        expect(json).not_to be_empty
-        # remove this line after the implementation of search function!
-        expect(json.size).to eq(10)
-      end
-
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
-      end
-    end
-  end
-
-  # Test suite for GET /tracks?track=song&{artist=a}
-  describe 'GET search song' do
-    # make HTTP get request before each example
-    before { get '/tracks', params: valid_attributes }
-
-    context 'when track parameter is present' do
-      it 'returns the result of the search' do
-        # Note `json` is a custom helper to parse JSON responses
-        expect(json).not_to be_empty
-        # assume 5 tracks at least
-        expect(json.size).to be > 5
-      end
-
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
-      end
+    it 'returns the most recent charts (10 songs now!)' do
+      expect(json).not_to be_empty
+      # remove this line after the implementation of search function!
+      expect(json.size).to eq(10)
     end
 
-    context 'when track parameter is NOT present' do
-      before { get '/tracks', params: invalid_attributes }
-
-      it 'returns status code 400' do
-        expect(response).to have_http_status(400)
-      end
-
-      it 'returns a validation failure message' do
-        expect(response.body)
-          .to match(/param is missing or the value is empty: track/)
-      end
-    end
-  end
-
-  # Test suite for GET /tracks/info?track=song&artist=a
-  describe 'GET track infos' do
-    # make HTTP get request before each example
-    before { get '/tracks/info', params: info_attributes }
-
-    context 'when both track and artist parameters are present' do
-      it 'returns the result of the search' do
-        # Note `json` is a custom helper to parse JSON responses
-        expect(json).not_to be_empty
-        # FIXME: parse result to see details!
-        expect(json.size).to eq(1)
-      end
-
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
-      end
-    end
-
-    context 'when a required parameter is NOT present' do
-      before { get '/tracks/info', params: invalid_attributes }
-
-      it 'returns status code 400' do
-        expect(response).to have_http_status(400)
-      end
-
-      it 'returns a validation failure message' do
-        expect(response.body)
-          .to match(/param is missing or the value is empty: artist/)
-      end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
     end
   end
 end
