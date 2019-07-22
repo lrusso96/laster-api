8f4f912e443ca01ceb1a05fee63b8d75400f7b07
luigi <russo.1699981@studenti.uniroma1.it>
2019-07-22 10:35:02 +0200

    autoload for laster module

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module LasterApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.enable_dependency_loading = true
+    config.autoload_paths << Rails.root.join('app/controllers/api')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
