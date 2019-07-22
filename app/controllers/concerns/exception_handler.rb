ac265abfb44bc3cf60d2280b571cfeddcca33b74
luigi <russo.1699981@studenti.uniroma1.it>
2019-07-22 11:06:15 +0200

    code cleanup

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -8,6 +8,7 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
+=begin
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
@@ -15,6 +16,7 @@ module ExceptionHandler
     rescue_from ActiveRecord::RecordInvalid do |e|
       json_response({ message: e.message }, :unprocessable_entity)
     end
+=end
 
     rescue_from ActionController::ParameterMissing do |e|
       json_response({ message: e.message }, :bad_request)
